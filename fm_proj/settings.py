0491e2a2b23babf12dd542cf1672b8a888c1ec89
dhcrain <dhcrain@gmail.com>
2016-07-25 11:31:17 -0400

    remove user media from requirements.txt

--- fm_proj/settings.py
+++ fm_proj/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'review_app',
     'hvad',
     'review',
-    'user_media',
+    # 'user_media',
     'generic_positions',
 ]
 
@@ -130,9 +130,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') # errorrs on
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') # errors on
 # http://stackoverflow.com/questions/26829435/collectstatic-command-fails-when-whitenoise-is-enabled
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  #errorrs on user_media/css/img/Jcrop.gif
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  #errors on user_media/css/img/Jcrop.gif
 
 STATIC_URL = '/static/'
 
