02d892247d361e0cdfba480654af14bf284041c2
dhcrain <dhcrain@gmail.com>
2016-07-25 09:41:39 -0400

    revert changes to setting.py

--- fm_proj/settings.py
+++ fm_proj/settings.py
@@ -129,8 +129,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') # errorrs on 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  #errorrs on user_media/css/img/Jcrop.gif
 
 STATIC_URL = '/static/'
 
